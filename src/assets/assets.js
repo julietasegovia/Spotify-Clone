97227cdf61292b3c5deee16960bd6e8f1fca3ab3
ITXUser <fer@sebastians-Mac-mini.local>
2026-04-21 10:05:02 -0300

    starting with sidebar

---
+++ src/assets/assets.js
@@ -0,0 +1,163 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
+import img1 from './img1.png'
+import img2 from './img2.png'
+import img3 from './img3.png'
+import img4 from './img4.png'
+import img5 from './img5.png'
+import img6 from './img6.jpg'
+import img7 from './img7.png'
+import img8 from './img8.jpg'
+import img12 from './img12.png'
+import img13 from './img13.jpg'
+import img14 from './img14.png'
+import img15 from './img15.jpg'
+import img16 from './img16.jpg'
+import song1 from  './song1.mp3'
+import song2 from  './song2.mp3'
+import song3 from  './song3.mp3'
+import song4 from  './song4.mp3'
+import song5 from  './song5.mp3'
+import song6 from  './song6.mp3'
+import song7 from  './song7.mp3'
+import song8 from  './song8.mp3'
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon
+}
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Top 50 Global",
+        image: img8,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#2a4365"
+    },
+    {   
+        id:1,
+        name: "Trending Global",
+        image: img16,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#44337a"
+    },
+    {   
+        id:2,
+        name: "Happy Favorites",
+        image: img15,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#744210"
+    }
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "Kill Bill",
+        image: img1,
+        file:song1,
+        desc:"SZA • SOS",
+        duration:"2:36"
+    },
+    {
+        id:1,
+        name: "Charmless Man - 2012 Remaster",
+        image: img2,
+        file:song2,
+        desc:"Blur • The Great Escape",
+        duration:"3:34"
+    },
+    {
+        id:2,
+        name: "The Walk",
+        image: img3,
+        file:song3,
+        desc:"The Cure • Greatest Hits",
+        duration:"3:31"
+    },
+    {
+        id:3,
+        name: "Sunday",
+        image: img4,
+        file:song1,
+        desc:"The Cranberries • Everyone Else Is Doing It, So Why Can't We?",
+        duration:"3:31"
+    },
+    {
+        id:4,
+        name: "Don't Speak",
+        image: img5,
+        file:song2,
+        desc:"No Doubt • Tragic Kingdom",
+        duration:"4:20"
+    },
+    {
+        id:5,
+        name: "Zoom",
+        image: img14    ,
+        file:song3,
+        desc:"Soda Stereo • Obras Cumbres (Parte 2)",
+        duration:"3:28"
+    },
+    {
+        id:6,
+        name: "Madwoman",
+        image: img7,
+        file:song1,
+        desc:"Laufey • A Matter Of Time: The Final Hour",
+        duration:"3:59"
+    },
+    {
+        id:7,
+        name: "Ritmo y Decepción",
+        image: img12,
+        file:song2,
+        desc:"Miranda • Magistral",
+        duration:"3:21"
+    }
+]
