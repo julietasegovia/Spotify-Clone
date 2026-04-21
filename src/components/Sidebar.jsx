4b4d1295c17ef40b504b59f2c51895dbf4d0d983
ITXUser <fer@sebastians-Mac-mini.local>
2026-04-21 10:36:01 -0300

    sidebar finished

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,6 +20,20 @@ const Sidebar = () => {
                         <img className='w-8' src={assets.stack_icon}/>
                         <p className='font-semibold'>Your Library</p>
                     </div>
+                    <div className='flex intems-center gap-3'>
+                        <img className='w-6 h-6' src={assets.arrow_icon}/>
+                        <img className='w-6 h-6' src={assets.plus_icon}/>
+                    </div>
+                </div>
+                <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start' gap-1 pl-4>
+                    <h1>Create your first playlist</h1>
+                    <p className='font-light'>It's easy, we'll help you.</p>
+                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Ceate Playlist</button>
+                </div>
+                <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start' gap-1 pl-4 mt-4>
+                    <h1>Find your favorite podcasts</h1>
+                    <p className='font-light'>We'll keep you updated on new episodes.</p>
+                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse Podcasts</button>
                 </div>
             </div>
         </div>
