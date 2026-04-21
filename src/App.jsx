faac8da1a554fc708a921082d8b1b926da108f75
ITXUser <fer@sebastians-Mac-mini.local>
2026-04-21 20:54:32 -0300

    fixing vite.config

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import React from "react"
 import Sidebar from "./components/Sidebar.jsx"
 import Player from "./components/Player.jsx"
 import Display from "./components/Display.jsx"
+import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='h-screen bg-black'>
+    <BrowserRouter basename="/Spotify-Clone/">
+      <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <Sidebar />
         <Display />
       </div>
       <Player />
     </div>
+    </BrowserRouter>
   )
 }
 
