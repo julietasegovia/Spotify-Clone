16cecda5c424e31318ecc943eeb01eadd7a93c4d
julietasegovia <julietavsegovia@gmail.com>
2026-04-22 22:41:04 -0300

    working player bar and buttons

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { PlayerContext } from '../context/PlayerContext'
 
 const SongItem = ({image, name, desc, id}) => {
 
-    const navigate = useNavigate();
+    const {playWithId} = useContext(PlayerContext)
 
     return (
-        <div className='min-w-[280px] p-3 px-4 rounded cursor-pointer hover:bg-[#ffffff26]'>
+        <div onClick={()=>playWithId(id)} className='min-w-[280px] p-3 px-4 rounded cursor-pointer hover:bg-[#ffffff26]'>
             <img className='rounded w-full aspect-square object-cover' src={image} />
             <p className='font-bold mt-3 mb-1 text-lg'>{name}</p>
             <p className='text-slate-200 text-sm'>{desc}</p>            
